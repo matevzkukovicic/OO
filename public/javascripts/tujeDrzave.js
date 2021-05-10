0649d6e3f8f780acc9c954c1b6045db7b824df6d
matevzkukovicic <mk2738@student.uni-lj.si>
2021-05-10 16:35:42 +0200

    dodane se delne omejitve prehoda

--- public/javascripts/tujeDrzave.js
+++ public/javascripts/tujeDrzave.js
@@ -1,20 +1,27 @@
 
 
 
-function onC(prehodne){
+function onC(prehodne,polprehodne){
     var options = document.getElementById("selectDrzavaID");
     var id  = (options.selectedIndex !== -1) ? options[options.selectedIndex].id : null;
     var index = parseInt(id.replace("drzava_",""));
-    if(prehodne.includes(index))
-       displayText(true)
-    else
-        displayText(false)
+    if(prehodne.includes(index)){
+        displayText(0)
+    }
+    else if (polprehodne.includes(index)){
+        displayText(1)
+    }else{
+        displayText(2)
+
+    }
 }
 
-function displayText(bool){
-    if(bool)
+function displayText(textCode){
+    if(textCode === 0)
         $( "#displayPrehodnost" ).html("<p id=\"prehodJeMogoc\" class=\"btn btn-light col-4 \">Prehod je mogoč!</p>");
-    else
+    else if (textCode === 1)
+        $( "#displayPrehodnost" ).html("<p id=\"prehodDelnoMogoc\" class=\"btn btn-light col-4 \">Prehod delno mogoč!</p>");
+    else if(textCode ===2)
         $( "#displayPrehodnost" ).html("<p id=\"prehodNiMogoc\" class=\"btn btn-light col-4 \">Prehod ni mogoč!</p>");
 
 }
